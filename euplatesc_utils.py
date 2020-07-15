e7fe8c9a519da7c5bd3c3f6eefa44a2fcdfd8fba
Ajay <43033303+AjaySRa@users.noreply.github.com>
2020-07-15 22:04:29 +0300

    Compute fp_hash using hmac module
    replaced hashlib module to hmac. 
    Decode key with codecs module in hex bytes object before passing it to hmacmd5()
    Pad key with bytes b'\0' to match the blocksize. 
    hmac.new(key,msg=None,digestmod) takes key in bytes or bytes array so no encoding is required for key. 
    data needs to be encoded in utf-8 unicode string before passing as msg to hmac.new()
    return hexdigest() all characters upper. 
    **Remark Python3.8.x doesn't use md5 as default method, so it should be passed as param to hmac.new() call.

--- euplatesc_utils.py
+++ euplatesc_utils.py
@@ -1,25 +1,16 @@
-import hashlib
+import hmac
 import uuid
+import codecs
 from time import strftime
+from urllib.parse import urlencode
+
 
 def hmacmd5(key, data):
     blocksize=64
-    key=key.ljust(blocksize, "\0")
-    ipad = chr(0x36)*blocksize
-    opad = chr(0x5C)*blocksize
-    p1 = ''.join(chr(ord(a) ^ ord(b)) for a,b in zip(key,opad))
-    p2 = ''.join(chr(ord(a) ^ ord(b)) for a,b in zip(key,ipad))
-    #create new hashlib md5 object
-    m1 = hashlib.md5()
-    m1.update(p2.encode('utf-8'))
-    m1.update(data.encode('utf-8'))
-    p3 = m1.digest()
-    #create second hashlib md5 object
-    m2 = hashlib.md5()
-    m2.update(p1.encode('utf-8'))
-    m2.update(p3)
-    return m2.hexdigest().upper()
-
+    #pad key as per requirement of hmac algorithm with blocksize using bytes b'\0'
+    key=key.ljust(blocksize, b"\0")
+    #encode data to unicode 8 bit
+    return hmac.new(key,data.encode('utf-8')).hexdigest().upper()
 
 def euplatesc_mac(key,params):
     data=""
@@ -28,8 +19,8 @@ def euplatesc_mac(key,params):
             data+="-"
         else:
             data+=str(len(p))+p
-    print(data)
-    return hmacmd5(key.encode('utf-8').hex(),data)
+    #call hmacmd5 by converting to hex bytes object
+    return hmacmd5(codecs.decode(key,"hex"),data)
 
 
 key="00112233445566778899AABBCCDDEEFF"
@@ -48,15 +39,17 @@ oparam=[
     params['amount'],
     params['curr'],
     params['invoice_id'],
-    params['merch_id'],
     params['order_desc'],
+    params['merch_id'],
     params['timestamp'],
     params['nonce']
 ]
 
 params['fp_hash']=euplatesc_mac(key,oparam)
 
-params['fname']="test";
-params['lname']="euplatesc"
-params['email']="test@euplatesc.ro"
-params['ExtraData[successurl]']="http://127.0.0.1:8000/payment/success"
+print(params['fp_hash'])
+#now show the customer the form
+#<form method="POST" action="https://secure.euplatesc.ro/tdsprocess/tranzactd.php">
+#print the inputs with the values from the var params
+#</form>
+print("https://secure.euplatesc.ro/tdsprocess/tranzactd.php?"+urlencode(params))
